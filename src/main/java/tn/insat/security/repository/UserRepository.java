5661dfeefd78f53666ec09c15d0cc50c434927b1
ziedYazidi <ziedyazidi75@gmail.com>
2018-04-14 15:31:35 +0200

    Set up for continous Integration

--- src/main/java/tn/insat/security/repository/UserRepository.java
+++ src/main/java/tn/insat/security/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package tn.insat.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.insat.model.security.User;
 
 /**
  * Created by stephan on 20.03.16.
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
